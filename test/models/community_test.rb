e4837cf0ac85baf9240b5781e532d6e6b4d23984
Jarom Rial <jarom@firebirdtech.dev>
2024-03-08 16:13:26 -0700

    add test community should not save without a name

--- test/models/community_test.rb
+++ test/models/community_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class CommunityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a community without a name" do
+    community = Community.new
+    assert_not community.save, "Saved the community without a name"
+  end
 end
