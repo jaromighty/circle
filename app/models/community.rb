9d0e1d5136040bb0d60c0e7709eb0cb0d0e8b363
Jarom Rial <jarom@firebirdtech.dev>
2024-03-08 16:17:27 -0700

    set slug for community after validation

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,10 @@
 class Community < ApplicationRecord
   validates :name, presence: true
+  after_validation :set_slug, only: [:create, :update]
+
+  private
+
+  def set_slug
+    self.slug = name.to_s.parameterize
+  end
 end
